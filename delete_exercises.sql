bd6de857a8f6a1f98f51e9c6cf92eaf2111771fa
Christina Antonuccio <antonuccio.christina@gmail.com>
2023-03-01 15:29:48 -0600

    db pt 2

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -3,15 +3,15 @@ USE ymir_christina;
 
 
 
-SELECT QUOTE('Albums released after 1991.');
-SELECT * FROM albums WHERE release_date > 1990;
+SELECT QUOTE('Albums released after in 1967.');
+DELETE FROM albums WHERE release_date = 1967;
 
 
 
-SELECT QUOTE('Albums with the genre ''disco''');
-SELECT * FROM albums WHERE genre = 'disco';
+SELECT QUOTE('Albums with the genre ''stuff''');
+DELETE FROM albums WHERE genre = 'stuff';
 
 
 
-SELECT QUOTE('Albums by ''Whitney Houston'' (...or maybe an artist of your choice).');
-SELECT * FROM albums WHERE artist = 'Whitney Houston';
+SELECT QUOTE('Albums by ''Metallica''.');
+DELETE FROM albums WHERE artist = 'Metallica';
